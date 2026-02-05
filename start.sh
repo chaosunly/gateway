ebd4859dd32d46114287d1699916ae6e3c6bcef5
Sunly <sunly.chao@rexform.io>
2026-02-06 00:27:38 +0700

    roll back

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-# Set defaults for optional variables if not provided
-: ${ADMIN_UI_INTERNAL:=http://ory-admin-ui.railway.internal:8080}
-
 # Render nginx config using env vars
-envsubst '${PORT} ${KRATOS_INTERNAL} ${KRATOS_ADMIN_INTERNAL} ${ADMIN_UI_INTERNAL} ${UI_INTERNAL}' \
+envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL}' \
   < /etc/nginx/templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
