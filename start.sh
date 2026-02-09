70d789b7a35b1fbdb4f1b5b9baabda26eda90345
Sunly <sunly.chao@rexform.io>
2026-02-09 11:23:16 +0700

    add keto

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Render nginx config using env vars
-envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL}' \
+envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${KETO_INTERNAL}' \
   < /etc/nginx/templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
