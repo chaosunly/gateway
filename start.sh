d3296bb6a9860543fb5bebb2583f70e971c51f13
Sunly <sunly.chao@rexform.io>
2026-02-10 10:50:25 +0700

    config keto url

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+# Default UI_PORT if not set (Next.js default is 8080)
+export UI_PORT="${UI_PORT:-8080}"
+
 # Render nginx config using env vars
-envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${KETO_INTERNAL}' \
+envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${UI_PORT} ${KETO_INTERNAL}' \
   < /etc/nginx/templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
