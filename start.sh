553cf8893d80136ca9158581f38ce88ab6759bd2
Sengly456 <senglyou84@gmail.com>
2026-02-12 14:29:52 +0700

    add hydra to start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-# Default UI_PORT if not set (Next.js default is 8080)
 export UI_PORT="${UI_PORT:-8080}"
 
-# Render nginx config using env vars
-envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${UI_PORT} ${KETO_INTERNAL}' \
+envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${UI_PORT} ${KETO_INTERNAL} ${HYDRA_INTERNAL}' \
   < /etc/nginx/templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
