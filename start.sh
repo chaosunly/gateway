82c5f5f6b99a568cf77810b05505c7ccc7d3c2f5
Sunly <sunly.chao@rexform.io>
2026-02-23 11:10:32 +0700

    Fix DNS resolver for Railway environment

--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@ set -e
 # Default UI_PORT if not set (Next.js default is 8080)
 export UI_PORT="${UI_PORT:-8080}"
 
+# Get DNS resolver from /etc/resolv.conf (Railway's DNS server)
+export DNS_RESOLVER=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
+echo "Using DNS resolver: ${DNS_RESOLVER}"
+
 # Render nginx config using env vars
-envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${UI_PORT} ${KETO_INTERNAL}' \
+envsubst '${PORT} ${KRATOS_INTERNAL} ${UI_INTERNAL} ${UI_PORT} ${KETO_INTERNAL} ${DNS_RESOLVER}' \
   < /etc/nginx/templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
