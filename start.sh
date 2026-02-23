8e8026150a45264e464a4a5cd7bf8354f24a484c
Sunly <sunly.chao@rexform.io>
2026-02-23 11:12:21 +0700

    Fix IPv6 DNS resolver handling

--- start.sh
+++ start.sh
@@ -5,7 +5,15 @@ set -e
 export UI_PORT="${UI_PORT:-8080}"
 
 # Get DNS resolver from /etc/resolv.conf (Railway's DNS server)
-export DNS_RESOLVER=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
+DNS_SERVER=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
+
+# Wrap IPv6 addresses in brackets for nginx
+if echo "$DNS_SERVER" | grep -q ':'; then
+  export DNS_RESOLVER="[${DNS_SERVER}]"
+else
+  export DNS_RESOLVER="${DNS_SERVER}"
+fi
+
 echo "Using DNS resolver: ${DNS_RESOLVER}"
 
 # Render nginx config using env vars
